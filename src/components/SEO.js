f8fe7b9690e48745418ba84e2be84b21df421020
cardfaux <cardfaux@tutanota.com>
2021-02-26 17:19:35 -0600

    updated the SEO component

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,9 +1,51 @@
-import React from "react"
-import { Helmet } from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
 
-const SEO = () => {
-  return <div>SEO Component</div>
-}
+const query = graphql`
+  {
+    site {
+      siteMetadata {
+        author
+        siteDesc: description
+        image
+        siteUrl
+        siteTitle: title
+        twitterUsername
+      }
+    }
+  }
+`;
+const SEO = ({ title, description }) => {
+  const { site } = useStaticQuery(query);
+  const {
+    siteDesc,
+    siteTitle,
+    siteUrl,
+    image,
+    twitterUsername,
+  } = site.siteMetadata;
 
-export default SEO
+  return (
+    <Helmet htmlAttributes={{ lang: 'en' }} title={`${title} | ${siteTitle}`}>
+      <meta name="description" content={description || siteDesc} />
+      <meta name="image" content={image} />
+      {/* FACEBOOK CARD */}
+      <meta property="og:url" content={siteUrl} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={siteTitle} />
+      <meta property="og:description" content={siteDesc} />
+      <meta property="og:image" content={`${siteUrl}${image}`} />
+      <meta property="og:image:width" content="400" />
+      <meta property="og:image:height" content="300" />
+      {/* twitter cards */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content={twitterUsername} />
+      <meta name="twitter:title" content={siteTitle} />
+      <meta name="twitter:description" content={siteDesc} />
+      <meta name="twitter:image" content={`${siteUrl}${image}`} />
+    </Helmet>
+  );
+};
+
+export default SEO;
