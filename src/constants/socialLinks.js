d85dc523b1ecd80be06a3269c2df7ee5547e42b5
cardfaux <cardfaux@tutanota.com>
2021-03-23 13:07:19 -0500

    added new social icons

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -1,51 +1,51 @@
-import React from "react"
+import React from 'react';
+import { SiMinds } from 'react-icons/si';
 import {
   FaFacebookSquare,
   FaLinkedin,
-  FaDribbbleSquare,
-  FaBehanceSquare,
   FaTwitterSquare,
-} from "react-icons/fa"
+  FaGithubSquare,
+} from 'react-icons/fa';
 
 const data = [
   {
     id: 1,
     icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
-    url: "https://www.twitter.com",
+    url: 'https://www.facebook.com/JamesHagoodDev',
   },
   {
     id: 2,
     icon: <FaLinkedin className="social-icon"></FaLinkedin>,
-    url: "https://www.twitter.com",
+    url: 'https://www.linkedin.com/in/james-hagood/',
   },
   {
     id: 3,
-    icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
-    url: "https://www.twitter.com",
+    icon: <FaGithubSquare className="social-icon"></FaGithubSquare>,
+    url: 'https://github.com/cardfaux',
   },
   {
     id: 4,
-    icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
-    url: "https://www.twitter.com",
+    icon: <SiMinds className="social-icon"></SiMinds>,
+    url: 'https://www.minds.com/cardfaux/',
   },
   {
     id: 5,
     icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
-    url: "https://www.twitter.com",
+    url: 'https://twitter.com/JamesHagoodDD',
   },
-]
-const links = data.map(link => {
+];
+const links = data.map((link) => {
   return (
     <li key={link.id}>
       <a href={link.url} className="social-link">
         {link.icon}
       </a>
     </li>
-  )
-})
+  );
+});
 
 export default ({ styleClass }) => {
   return (
-    <ul className={`social-links ${styleClass ? styleClass : ""}`}>{links}</ul>
-  )
-}
+    <ul className={`social-links ${styleClass ? styleClass : ''}`}>{links}</ul>
+  );
+};
