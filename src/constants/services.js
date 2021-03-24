9bc313c353a780cdaf5f10ed56082c2c5fd99f6b
cardfaux <cardfaux@tutanota.com>
2021-03-24 13:00:32 -0500

    made a change to the constants/services

--- src/constants/services.js
+++ src/constants/services.js
@@ -1,22 +1,22 @@
-import React from "react"
-import { FaCode, FaSketch, FaAndroid } from "react-icons/fa"
+import React from 'react';
+import { FaCode, FaSketch, FaShopify } from 'react-icons/fa';
 export default [
   {
     id: 1,
     icon: <FaCode className="service-icon" />,
-    title: "web development",
+    title: 'web development',
     text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
   },
   {
     id: 2,
     icon: <FaSketch className="service-icon" />,
-    title: "web design",
+    title: 'web design',
     text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
   },
   {
     id: 3,
-    icon: <FaAndroid className="service-icon" />,
-    title: "app design",
+    icon: <FaShopify className="service-icon" />,
+    title: 'ecommerce development',
     text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
   },
-]
+];
