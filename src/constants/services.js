65330e79633bca4c4df5bde8e0a096b59ea14418
cardfaux <cardfaux@tutanota.com>
2021-03-25 14:04:26 -0500

    still not working

--- src/constants/services.js
+++ src/constants/services.js
@@ -5,18 +5,18 @@ export default [
     id: 1,
     icon: <FaCode className="service-icon" />,
     title: 'web development',
-    text: `as a web developer I am responsible for writing all the code that is responsible for making your web site or application operate as you expect it to.`,
+    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
   },
   {
     id: 2,
     icon: <FaSketch className="service-icon" />,
     title: 'web design',
-    text: `I use figma as my web design weapon of choice, I use it for everything from wire frames to high fidelity mockups. I go through the whole process with you from layout to production.`,
+    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
   },
   {
     id: 3,
     icon: <FaShopify className="service-icon" />,
     title: 'ecommerce development',
-    text: `since 2020 I have been helping small businesses impacted by the COVID-19 virus get their products online to their customers. Helping them get their stores online and bring in new revenue.`,
+    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
   },
 ];
